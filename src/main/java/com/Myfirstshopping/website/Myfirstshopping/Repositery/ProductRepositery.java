be5a587ee98148818ffe0f88ea995c215b7c3a91
Basant <basantdhiman9@gmail.com>
2024-10-15 11:34:50 +0530

    create function to get all product of a seller

--- src/main/java/com/Myfirstshopping/website/Myfirstshopping/Repositery/ProductRepositery.java
+++ src/main/java/com/Myfirstshopping/website/Myfirstshopping/Repositery/ProductRepositery.java
@@ -3,9 +3,15 @@ package com.Myfirstshopping.website.Myfirstshopping.Repositery;
 
 import com.Myfirstshopping.website.Myfirstshopping.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepositery extends JpaRepository<Product, UUID> {
+
+
+    @Query(value = "select * from product where seller_id=:sellerID",nativeQuery = true)
+    public List<Product> getAllProductBySellerID(UUID sellerID);
 
 }
--- src/main/java/com/Myfirstshopping/website/Myfirstshopping/Service/SellerService.java
+++ src/main/java/com/Myfirstshopping/website/Myfirstshopping/Service/SellerService.java
@@ -1,7 +1,10 @@
 package com.Myfirstshopping.website.Myfirstshopping.Service;
 
 
+
 import com.Myfirstshopping.website.Myfirstshopping.Repositery.ProductRepositery;
+import com.Myfirstshopping.website.Myfirstshopping.ResponseBody.ProductResponseBody;
+import com.Myfirstshopping.website.Myfirstshopping.ResponseBody.UserResponseBody;
 import com.Myfirstshopping.website.Myfirstshopping.exception.AccessNotFound;
 import com.Myfirstshopping.website.Myfirstshopping.exception.InvalidProductID;
 import com.Myfirstshopping.website.Myfirstshopping.exception.UserNotFound;
@@ -11,6 +14,8 @@ import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 @ToString
 @Service
@@ -57,6 +62,39 @@ public class SellerService {
        productService.removeProduct(product);
        return String.format("USer with name %s removed product with id %",owner.getName(),product.getProductName());
     }
+    public List<ProductResponseBody> getAllProductBySellerID(UUID sellerID){
+        List<Product> products = productRepositery.getAllProductBySellerID(sellerID);
+        List<ProductResponseBody> productResponseBodyList = new ArrayList<>();
+
+        for(Product product : products){
+            AppUser seller = product.getSeller();
+            ProductResponseBody productResponseBody = new ProductResponseBody();
+            UserResponseBody userResponseBody = new UserResponseBody();
+            // Setting product values inside product response body
+            productResponseBody.setProductName(product.getProductName());
+            productResponseBody.setCategory(product.getCategory());
+            productResponseBody.setPrice(product.getPrice());
+            productResponseBody.setRating(product.getRating());
+            productResponseBody.setQuantity(product.getQuantity());
+            // Setting user values inside user response body
+            userResponseBody.setAddress(seller.getAddress());
+            userResponseBody.setAge(seller.getAge());
+            userResponseBody.setName(seller.getName());
+            userResponseBody.setEmail(seller.getEmail());
+            userResponseBody.setPhoneNumber(seller.getPhoneNumber());
+            // Set userResponseBody inside product response body
+            productResponseBody.setUserResponseBody(userResponseBody);
+            // For a specific product product response body is built we need to add product response body in list
+
+            productResponseBodyList.add(productResponseBody);
+        }
+
+        return productResponseBodyList;
+    }
+
+
+
+
 
 
 
